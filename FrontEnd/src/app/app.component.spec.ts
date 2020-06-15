c63199c331cb2c0c9a852da1f83ed11888776937
unknown <pareeknikhil397@gmail.com>
2020-06-15 11:34:35 +0530

    adding some layout

--- FrontEnd/src/app/app.component.spec.ts
+++ FrontEnd/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'FrontEnd'`, () => {
+  it(`should have as title 'frontEnd'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('FrontEnd');
+    expect(app.title).toEqual('frontEnd');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('FrontEnd app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('frontEnd app is running!');
   });
 });
