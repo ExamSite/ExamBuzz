77a1d980b6b0061110598a05f585cdb1673ddb8b
MARCOS <ayushjain.skj@gmail.com>
2020-06-20 13:54:27 +0530

    fix

--- backend/index.js
+++ backend/index.js
@@ -6,31 +6,23 @@ const ObjectId = require('mongodb').ObjectId;
 
 var dbName = "GrrasProject"
 
-// var client = new MongoClient( 'mongodb://localhost:27017/mypro', {useNewUrlParser:true});
-var client = new MongoClient('mongodb+srv://root:root@cluster.i5ume.gcp.mongodb.net/dbName?retryWrites=true&w=majority', { useNewUrlParser: true });
-
-
-
-
-var connection;
-client.connect((err, con)=>{
-
-        if(!err)
-        {
-                connection = con;
-                console.log("database connected successfully");
-        }
-        else{
-            console.log("database could not connect");
-        }
-})
-
-
-
 const app = express();
 
 app.use(cors());
 
+// var client = new MongoClient( 'mongodb://localhost:27017/mypro', {useNewUrlParser:true});
+var client = new MongoClient('mongodb+srv://root:root@cluster.i5ume.gcp.mongodb.net/dbName?retryWrites=true&w=majority', { useNewUrlParser: true });
+
+var connection;
+client.connect((err, con) => {
+    if (!err) {
+        connection = con;
+        console.log("database connected successfully");
+    }
+    else {
+        console.log("database could not connect");
+    }
+})
 
 /*app.get('/', (req, res)=>{
 
@@ -46,64 +38,42 @@ app.get('/user', (req, res)=>{
 })*/
 
 
-app.post('/sign-in', bodyParser.json() ,(req,res)=>{
-        var collection = connection.db(dbName).collection('Users');
-
-        collection.find(req.body).toArray((err,docs)=>{
-            if(!err && docs.length>0)
-            {
-                res.send({ status:"ok", data:docs });
-            }
-            else{
-                res.send({status:"failed", data:err});
-            }
-        })
-})
-
-
-
-app.post('/sign-up', bodyParser.json() ,(req,res)=>{
+app.post('/sign-in', bodyParser.json(), (req, res) => {
     var collection = connection.db(dbName).collection('Users');
-    collection.find({email:req.body.email}).toArray((err,docs)=>{
-    if(!err && docs.length>0)
-    {
-       res.send({status:"failed", data:"email already Exist"})
-    }
-    else{
-        collection.insert(req.body, (err,result)=>{
-            if(!err)
-            {
-                res.send({ status:"ok", data:"signup success" });
+    collection.find(req.body).toArray((err, docs) => {
+        if (!err && docs.length > 0) {
+            res.send({ status: "ok", data: docs });
+        }
+        else {
+            res.send({ status: "failed", data: err });
+        }
+    })
+})
+
+
+
+app.post('/sign-up', bodyParser.json(), (req, res) => {
+    var collection = connection.db(dbName).collection('Users');
+    
+    //collection.find({ email: req.body.email }).toArray((err, docs) => {
+        // if(!err && docs.length>0)
+        // {
+        //     res.send({status:"failed", data:"email already Exist"})
+        // }
+        // else
+        // {
+        collection.insert(req.body, (err, result) => {
+            if (!err) {
+                res.send({ status: "ok", data: "signup success" });
             }
-            else{
-                res.send({status:"failed", data:err});
+            else {
+                res.send({ status: "failed", data: err });
             }
         })
-    }
+        // }
+    //})
 })
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-})
-
-
-
-
-
-
-
-
-app.listen(5000, ()=>{console.log("server is listining on port 5000")});
+app.listen(5000, () => { console.log("server is listining on port 5000") });
